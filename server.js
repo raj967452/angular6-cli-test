d76366b022deba48d3470376975bcdc4e341cf18
DESKTOP-UA7RFN3\Admin <rajkumar967452@gmail.com>
2018-12-05 00:04:34 +0530

    server js changes

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/angular6-cli-test'));
 
 app.get('/*', function(req,res) {    
-	res.sendFile(path.join(__dirname+'/dist/index.html'));
+	res.sendFile(path.join(__dirname+'/dist/angular6-cli-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
